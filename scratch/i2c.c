79940aac28e7898e909f359f46a6b025ece28e4b
Curtis Ly <137116222+lycurtis@users.noreply.github.com>
2025-06-27 13:21:27 -0700

    Update i2c.c

--- scratch/i2c.c
+++ scratch/i2c.c
@@ -1,10 +1,3 @@
-/*
- * i2c.c
- *
- *  Created on: Jun 23, 2025
- *      Author: cly
- */
-
 #include "i2c.h"
 
 // Include your STM32 register headers here
@@ -18,6 +11,7 @@
 #define AC_GPIOB_OTYPER_OT9 (1U<<9)
 #define I2C_100KHZ 80
 #define SD_MODE_MAX_RISE_TIME 17
+#define I2C1_AF4 (4U)
 
 void I2C_Init(void) {
 	// Enable clock access for GPIOB
@@ -31,26 +25,34 @@ void I2C_Init(void) {
 	GPIOB->MODER |= (1U<<19);
 	GPIOB->MODER &= ~(1U<<18);
 
+	// Configure Open-Drain output type
 	GPIOB->OTYPER |= AC_GPIOB_OTYPER_OT8; // Output open drain pin 8
 	GPIOB->OTYPER |= AC_GPIOB_OTYPER_OT9; // Output open drain pin 9
 
-	// Enable Pulll-up [01] for PB8 and PB9
+	// Enable Pull-up [01] for PB8 and PB9
 	GPIOB->PUPDR &= ~(1U<<17);
 	GPIOB->PUPDR |= (1U<<16);
 
 	GPIOB->PUPDR &= ~(1U<<19);
 	GPIOB->PUPDR |= (1U<<18);
 
-    // Configure RCC clock to I2C1 peripheral
+	// Configure Alternate Function I2C1 (AF4) for PB8 and PB9
+	// We know AF4 based on Alternate function mapping on datasheet
+	GPIOB->AFR[1] |= (4U << 0);  // PB8: AFRH[3:0]
+	GPIOB->AFR[1] |= (4U << 4);  // PB9: AFRH[7:4]
+
+
+    // Enable clock access to I2C1 peripheral
 	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
 
-    // Configure I2C timing registers
+    // RESET I2C1
 	// 1. Enter RESET Mode then immediately come out of RESET
 	I2C1->CR1 |= I2C_CR1_SWRST;
 	I2C1->CR1 &= ~I2C_CR1_SWRST;
 
 	// 2. Set clock Frequency to 16 MHz
-	I2C1->CR2 |= (1U<<4); //FREQ[5:0] ==> (1U<<4) = 0b0100 = 16
+	I2C1->CR2 = 16U; //I2C1->CR2 |= (1U<<4); //FREQ[5:0] ==> (1U<<4) = 0b0100 = 16
+
 
 	// 3. Set I2C to standard mode (100 kHz)
 	/*
@@ -73,8 +75,6 @@ void I2C_Init(void) {
 void I2C_Write(uint8_t dev_addr, uint8_t *data, uint16_t len) {
     // Implement I2C write sequence (START, SEND ADDR, WRITE DATA, STOP)
 
-	volatile uint32_t temp;
-
 	// 1. Generate a START condition (set the START bit in CR1)
 	I2C1->CR1 |= I2C_CR1_START;
 
@@ -88,8 +88,8 @@ void I2C_Write(uint8_t dev_addr, uint8_t *data, uint16_t len) {
 	while(!(I2C1->SR1 & I2C_SR1_ADDR)){}
 
 	// 5. Clear the ADDR flag by reading SR1 followed by SR2 (this is required)
-	temp = I2C1->SR1;
-	temp = I2C1->SR2;
+	(void)I2C1->SR1;
+	(void)I2C1->SR2;
 
 	// 6. Loop through the data array:
 	//    For each byte:
@@ -108,66 +108,47 @@ void I2C_Write(uint8_t dev_addr, uint8_t *data, uint16_t len) {
 
 void I2C_Read(uint8_t dev_addr, uint8_t *data, uint16_t len) {
     // Implement I2C read sequence (START, SEND ADDR, READ DATA, STOP)
-	volatile uint32_t temp;
 
-    // 1. Generate a START condition by setting the START bit in CR1
-	I2C1->CR1 |= I2C_CR1_START;
+	// 1. Enable ACK generation
+	    I2C1->CR1 |= I2C_CR1_ACK;
 
-    // 2. Wait until the SB (Start Bit) flag is set in SR1
-	while(!(I2C1->SR1 & I2C_SR1_SB)){}
+	    // 2. Generate a START condition
+		I2C1->CR1 |= I2C_CR1_START;
+		while(!(I2C1->SR1 & I2C_SR1_SB));
 
-    // 3. Send the slave address with the READ bit (LSB = 1)
-	I2C1->DR = dev_addr;
+	    // 3. Send the slave address with the READ bit (LSB = 1)
+	    // FIX: Address must be shifted and OR'd with 1 for read.
+		I2C1->DR = (dev_addr << 1) | 1;
+		while(!(I2C1->SR1 & I2C_SR1_ADDR));
 
-    // 4. Wait until the ADDR (Address Sent) flag is set in SR1
-	while(!(I2C1->SR1 & I2C_SR1_ADDR)){}
+	    // 4. Clear the ADDR flag
+		(void)I2C1->SR1;
+		(void)I2C1->SR2;
 
-    // 5. Clear the ADDR flag by reading SR1 then SR2 (required)
-	temp = I2C1->SR1;
-	temp = I2C1->SR2;
+	    // 5. Loop to read bytes
+	    for (int i = 0; i < len; i++) {
+	        if (i == len - 1) {
+	            // For the last byte, we need to NACK it and issue a STOP condition.
+	            // a. Disable ACK
+	            I2C1->CR1 &= ~I2C_CR1_ACK;
+	            // b. Generate STOP
+	            I2C1->CR1 |= I2C_CR1_STOP;
+	        }
 
-    // 6. If only ONE byte to read:
-	if(len == 1){
-		//    a. Clear the ACK bit in CR1 (NACK after the next byte)
-		I2C1->CR1 &= ~I2C_CR1_ACK;
+	        // Wait until RXNE (Receive Buffer Not Empty) flag is set
+	        while(!(I2C1->SR1 & I2C_SR1_RXNE));
 
-		//    b. Generate the STOP condition immediately
-		I2C1->CR1 |= I2C_CR1_STOP;
+	        // Read data from DR
+	        data[i] = I2C1->DR;
+	    }
 
-		//    c. Wait until RXNE (Receive Buffer Not Empty) flag is set
-		while(!(I2C1->SR1 & I2C_SR1_RXNE)){}
-
-		//    d. Read data from DR
-		*data = I2C1->DR;
-	}
-	else{
-		// 7. If MORE than one byte:
-		//    a. For all bytes except the last two:
-		for(int i = 0; i < len; i++){
-			while(!(I2C1->SR1 & I2C_SR1_RXNE)){} // Wait until RXNE is set
-
-			// if second to last byte
-			if(i == (len - 2)){
-				//    b. When only two bytes remain:
-				//       - Clear ACK bit (prepare NACK for second-to-last byte)
-				//       - Generate STOP condition
-				//       - Read remaining two bytes when RXNE is set
-				I2C1->CR1 &= ~I2C_CR1_ACK;
-				I2C1->CR1 |= I2C_CR1_STOP;
-			}
-			//READ DATA FROM DR INTO BUFFER
-			data[i] = I2C1->DR;
-		}
-	}
-
-    // 8. Make sure to re-enable ACK after the transfer if you disabled it
-	I2C1->CR1 |= I2C_CR1_ACK;
+	    // 6. Re-enable ACK for future transfers
+	    I2C1->CR1 |= I2C_CR1_ACK;
 }
+
 
 void I2C_Mem_Write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len) {
     // Send register address, then write data
-
-	volatile uint32_t temp;
 
 	// 1. Generate a START condition (set the START bit in CR1)
 	I2C1->CR1 |= I2C_CR1_START;
@@ -183,26 +164,29 @@ void I2C_Mem_Write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t l
 
 	// 5. Clear the ADDR flag by reading SR1 followed by SR2
 	// To clear this flag (which is required to continue the transfer):
-	temp = I2C1->SR1; // Read SR1
-	temp = I2C1->SR2; // Then immediately after read SR2
+	(void)I2C1->SR1; // Read SR1
+	(void)I2C1->SR2; // Then immediately after read SR2
 
 	// 6. Send the memory/register address (mem_addr)
+	while(!(I2C1->SR1 & I2C_SR1_TXE)); // Must wait for TXE before sending register address
 	I2C1->DR = reg_addr;
 
-	// 7. Wait until the TXE (Transmit Data Register Empty) flag is set
-	while(!(I2C1->SR1 & I2C_SR1_TXE)){}
+//	// 7. Wait until the TXE (Transmit Data Register Empty) flag is set
+//	while(!(I2C1->SR1 & I2C_SR1_TXE)){}
 
 	// 8. Loop through the data array:
 	//    For each byte:
-	//    a. Write the byte to the DR register
-	//    b. Wait until TXE is set
+	//    a. Wait until TXE is set
+	//    b. Write the byte to the DR register
 	for(int i = 0; i < len; i++){
-		I2C1->DR = data[i]; //a. Write the current byte to the DR register
 
 		while(!(I2C1->SR1 & I2C_SR1_TXE)){}
+
+		I2C1->DR = data[i]; // Write the current byte to the DR register
 	}
 
 	// 9. After all bytes are sent, wait until BTF (Byte Transfer Finished) flag is set (optional but recommended)
+	while(!(I2C1->SR1 & I2C_SR1_TXE)){}
 	while(!(I2C1->SR1 & I2C_SR1_BTF)){}
 
 	// 10. Generate a STOP condition (set the STOP bit in CR1)
@@ -221,84 +205,57 @@ void I2C_Mem_Read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t le
 		Tell the device which internal memory/register you want to read from.
 		Then actually read the data from that register.
 	 */
-	volatile uint32_t temp;
+	// Send register address, then read data (Combined Transaction)
 
-	// 1. Generate a START condition (set START in CR1)
-	I2C1->CR1 |= I2C_CR1_START;
+		// ====== Phase 1: Write the register address to read from ======
 
-	// 2. Wait until the SB (Start Bit) flag is set in SR1
-	while(!(I2C1->SR1 & I2C_SR1_SB)){}
+		// 1. Generate a START condition
+		I2C1->CR1 |= I2C_CR1_START;
+		while(!(I2C1->SR1 & I2C_SR1_SB));
 
-	// 3. Send the slave address with the WRITE bit (LSB = 0)
-	I2C1->DR = dev_addr << 1;
+		// 2. Send the slave address with the WRITE bit
+		I2C1->DR = dev_addr << 1;
+		while(!(I2C1->SR1 & I2C_SR1_ADDR));
 
-	// 4. Wait until the ADDR (Address Sent) flag is set in SR1
-	while(!(I2C1->SR1 & I2C_SR1_ADDR)){}
+		// 3. Clear the ADDR flag
+		(void)I2C1->SR1;
+		(void)I2C1->SR2;
 
-	// 5. Clear the ADDR flag by reading SR1 then SR2
-	temp = I2C1->SR1;
-	temp = I2C1->SR2;
+		// 4. Send the memory/register address
+		while(!(I2C1->SR1 & I2C_SR1_TXE));
+		I2C1->DR = reg_addr;
 
-	// 6. Send the memory/register address (mem_addr)
-	I2C1->DR = reg_addr;
+		// 5. Wait for TXE flag to set, indicating address is sent
+		while(!(I2C1->SR1 & I2C_SR1_TXE));
 
-	// 7. Wait until TXE (Transmit Buffer Empty) flag is set
-	while(!(I2C1->SR1 & I2C_SR1_TXE)){}
 
-	// 8. Generate a REPEATED START condition (set START in CR1 again)
-	I2C1->CR1 |= I2C_CR1_START;
+		// ====== Phase 2: Read data from the specified register ======
 
-	// 9. Wait until SB flag is set again
-	while(!(I2C1->SR1 & I2C_SR1_SB)){}
+		// 6. Generate a REPEATED START condition
+		I2C1->CR1 |= I2C_CR1_START;
+		while(!(I2C1->SR1 & I2C_SR1_SB));
 
-	// 10. Send the slave address with the READ bit (LSB = 1)
-	I2C1->DR = (dev_addr << 1) | 1;
+		// 7. Send the slave address with the READ bit
+		I2C1->DR = (dev_addr << 1) | 1;
+		while(!(I2C1->SR1 & I2C_SR1_ADDR));
 
-	// 11. Wait until ADDR flag is set
-	while(!(I2C1->SR1 & I2C_SR1_ADDR)){}
+		// 8. Clear the ADDR flag
+		(void)I2C1->SR1;
+		(void)I2C1->SR2;
 
-	// 12. Clear the ADDR flag by reading SR1 and SR2
-	temp = I2C1->SR1;
-	temp = I2C1->SR2;
+		// 9. Read the data bytes (using the same logic as I2C_Read)
+	    // FIX: Use the corrected read logic here as well.
+	    for (int i = 0; i < len; i++) {
+	        if (i == len - 1) {
+	            // For the last byte, NACK it and STOP.
+	            I2C1->CR1 &= ~I2C_CR1_ACK;
+	            I2C1->CR1 |= I2C_CR1_STOP;
+	        }
 
-	// 13. If ONE byte to read:
-	//     a. Clear ACK bit (prepare NACK after this byte)
-	//     b. Generate STOP condition
-	//     c. Wait for RXNE flag
-	//     d. Read the byte from DR
-	if(len == 1){
-		I2C1->CR1 &= ~I2C_CR1_ACK; // a. Clear ACK bit
-		I2C1->CR1 |= I2C_CR1_STOP; // b. Generate stop condition
-		while(!(I2C1->SR1 & I2C_SR1_RXNE)){} // c. Wait for RXNE flag
-		*data = I2C1->DR; // d. read the byte from DR
+	        while(!(I2C1->SR1 & I2C_SR1_RXNE));
+	        data[i] = I2C1->DR;
+	    }
 
-		//Re-enable ACK for future communications
-		I2C1->CR1 |= I2C_CR1_ACK;
-	}
-	else{ // 14. If MORE than one byte:
-		// a. For all bytes except the last two:
-		//    - Wait for RXNE flag
-		//    - Read data from DR into buffer
-		//     b. When two bytes remain:
-		//        - Clear ACK bit
-		//        - Generate STOP condition
-		//        - Read remaining two bytes as RXNE becomes set
-		for(int i = 0; i < len; i++){
-			if(i == (len - 2)){
-				// Prepare to NACK the second to last byte
-				I2C1->CR1 &= ~I2C_CR1_ACK;
-
-				//Send STOP condition
-				I2C1->CR1 |= I2C_CR1_STOP;
-			}
-
-			while(!(I2C1->SR1 & I2C_SR1_RXNE)){} // Wait for RXNE flag
-
-			//READ THE DATA
-			data[i] = I2C1->DR;
-		}
-
-		// 15. After transfer, re-enable ACK if it was disabled
-		I2C1->CR1 |= I2C_CR1_ACK;
-	}
+	    // 10. Re-enable ACK for future transfers
+	    I2C1->CR1 |= I2C_CR1_ACK;
 }
